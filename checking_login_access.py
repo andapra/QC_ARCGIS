8dd96fb8e4d52f2ad16fba1f4cfff94b1a7b97b6
andapra <41572610+andapra@users.noreply.github.com>
2023-08-22 12:06:30 +0700

    Update checking_login_access.py

--- checking_login_access.py
+++ checking_login_access.py
@@ -16,6 +16,7 @@ def check_web_access(url_page, wa_portal, wa_server, uname, pwd):
     driver = webdriver.Chrome(service=service)
     wait = WebDriverWait(driver, 10)
 
+    print('UAT Scenario for Portal')
     driver.get('{}/{}/home'.format(url_page, wa_portal))
 
     wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="header"]/div/div/div[2]/div[11]/div/button')))
@@ -46,7 +47,48 @@ def check_web_access(url_page, wa_portal, wa_server, uname, pwd):
     # wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="create-dropdown"]')))
     # driver.find_element(By.XPATH, '//*[@id="create-dropdown"]').click()
 
-    driver.get('{}/{}/home'.format(url_page, wa_server))
+    wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[4]/arcgis-new-item/calcite-modal/calcite-button[3]')))
+    driver.find_element(By.XPATH, '/html/body/div[4]/arcgis-new-item/calcite-modal/calcite-button[3]').click()
+
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="dijit__TemplatedMixin_0"]/div/nav/a[5]')))
+    driver.find_element(By.XPATH, '//*[@id="dijit__TemplatedMixin_0"]/div/nav/a[5]').click()
+
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="dijit__TemplatedMixin_53"]/button')))
+    driver.find_element(By.XPATH, '//*[@id="dijit__TemplatedMixin_53"]/button').click()
+
+    print('Please check manual federation server')
+
+    print('UAT Scenario for Portal is Completed')
+
+
+
+    print('Checking login access to ArcGIS Server scenarion')
+    driver.switch_to.new_window('tab')
+
+
+    print('UAT Scenario for ArcGIS Server is Started')
+    driver.get('{}/{}/manager'.format(url_page, wa_server))
+
+    time.sleep(10)
+
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="navtabs"]/div[1]/ul/li[2]')))
+    driver.find_element(By.XPATH, '//*[@id="navtabs"]/div[1]/ul/li[2]').click()
+
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="dataStoresLabel')))
+    driver.find_element(By.XPATH, '//*[@id="dataStoresLabel"]').click()
+
+    print('UAT Scenario for ArcGIS Server is Completed')
+
+
+    print('UAT Scenario for Datastore Validation started now')
+
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="data"]/div[3]/table/tbody/tr/td[1]/span[2]')))
+    driver.find_element(By.XPATH, '//*[@id="data"]/div[3]/table/tbody/tr/td[1]/span[2]').click()
+    time.sleep(60)
+
+    print('UAT Scenario for Datastore Validation started now')
+
+    print('All UAT Scenario is Completed')
 
 if __name__ == "__main__":
     url = input('Please input portal url example (https://machine.domain.com): ')
@@ -56,4 +98,7 @@ if __name__ == "__main__":
     web_adaptor_portal = input('Please input web adaptor portal: ')
     web_adaptor_server = input('Please input web adaptor server: ')
 
-    check_web_access(url, web_adaptor_portal, web_adaptor_server, uname, pwd)
+    try:
+        check_web_access(url, web_adaptor_portal, web_adaptor_server, uname, pwd)
+    except Exception as e:
+        raise(e)
